4f2b778be206e5d4864f7fe2ac182726541c2937
Michael Bull <mikebull94@gmail.com>
2025-08-11 11:44:09 +0100

    Sanity check interval in IntervalManager#add

--- src/main/kotlin/com/github/michaelbull/interval/IntervalManager.kt
+++ src/main/kotlin/com/github/michaelbull/interval/IntervalManager.kt
@@ -8,6 +8,8 @@ class IntervalManager {
      * Adds a new interval. Automatically merge overlapping or adjacent intervals.
      */
     fun addInterval(interval: Interval) {
+        checkInterval(interval)
+
         return TODO()
     }
 
@@ -16,5 +18,25 @@ class IntervalManager {
      */
     fun getIntervals(): List<Interval> {
         return TODO()
+    }
+
+    private fun checkInterval(interval: IntArray) {
+        require(interval.size == 2) {
+            "interval.size must be exactly 2, but was ${interval.size}"
+        }
+
+        val (start, end) = interval
+
+        require(start >= 0) {
+            "interval.start must be non-negative, but was $start"
+        }
+
+        require(end >= 0) {
+            "interval.end must be non-negative, but was $start"
+        }
+
+        require(end > start) {
+            "interval.end must be greater-than interval.start, but was start=${start} end=${end}"
+        }
     }
 }
