c9195a0adb548ab9f6248bf520f94deee1eb22e6
Michael Bull <mikebull94@gmail.com>
2025-08-11 12:52:17 +0100

    Implement IntervalManager#addInterval

--- src/main/kotlin/com/github/michaelbull/interval/IntervalManager.kt
+++ src/main/kotlin/com/github/michaelbull/interval/IntervalManager.kt
@@ -1,20 +1,67 @@
 package com.github.michaelbull.interval
 
+import java.util.NavigableSet
+import java.util.TreeSet
+
+private val compareIntervalsByStart = compareBy<Interval> { (start, _) -> start }
+private val INTERVAL_MIN = intArrayOf(INTERVAL_START_MIN, INTERVAL_END_INCLUSIVE_MIN)
+
 class IntervalManager {
 
+    private val intervals = sortedSetOf(compareIntervalsByStart)
+
     /**
-     * Adds a new interval. Automatically merge overlapping or adjacent intervals.
+     * Adds a new interval, merging with overlaps.
      */
     fun addInterval(interval: Interval) {
         checkInterval(interval)
 
-        return TODO()
+        var (start, endInclusive) = interval
+
+        if (intervals.isNotEmpty()) {
+            val overlapWindow = intervals.subsetUpTo(endInclusive)
+            val iterator = overlapWindow.iterator()
+
+            while (iterator.hasNext()) {
+                val (candidateStart, candidateEndInclusive) = iterator.next()
+
+                if (candidateStart > endInclusive) {
+                    /* the candidate starts after the new interval; no valid merge candidates remain */
+                    break
+                } else if (candidateEndInclusive >= start) {
+                    /* the candidate ends either before or as we start, merge with it */
+
+                    iterator.remove()
+
+                    start = start.coerceAtMost(candidateStart)
+                    endInclusive = endInclusive.coerceAtLeast(candidateEndInclusive)
+                }
+            }
+        }
+
+        intervals.add(intArrayOf(start, endInclusive))
     }
 
     /**
      * Returns the list of merged intervals, sorted by start time.
      */
     fun getIntervals(): List<Interval> {
-        return TODO()
+        return intervals.map(Interval::clone)
+    }
+
+    private fun TreeSet<Interval>.subsetUpTo(endInclusive: Int): NavigableSet<Interval> {
+        return inclusiveSubset(
+            fromInclusive = INTERVAL_MIN,
+            toInclusive = intArrayOf(endInclusive, INTERVAL_END_INCLUSIVE_MAX),
+        )
+    }
+
+    private fun <E> TreeSet<E>.inclusiveSubset(fromInclusive: E, toInclusive: E): NavigableSet<E> {
+        return subSet(
+            /* fromElement = */ fromInclusive,
+            /* fromInclusive = */ true,
+            /* toElement = */ toInclusive,
+            /* toInclusive = */ true,
+        )
     }
 }
--- src/test/kotlin/com/github/michaelbull/interval/IntervalManagerTest.kt
+++ src/test/kotlin/com/github/michaelbull/interval/IntervalManagerTest.kt
@@ -1,8 +1,8 @@
 package com.github.michaelbull.interval
 
 import kotlin.test.BeforeTest
-import kotlin.test.Ignore
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 
 class IntervalManagerTest {
@@ -15,15 +15,145 @@ class IntervalManagerTest {
     }
 
     @Test
-    @Ignore
     fun `first example`() {
         manager.addInterval(intArrayOf(1, 3))
         manager.addInterval(intArrayOf(5, 7))
         manager.addInterval(intArrayOf(2, 6))
 
+        val intervals = manager.getIntervals()
+
         assertEquals(
-            expected = listOf(intArrayOf(1, 7)),
-            actual = manager.getIntervals(),
+            expected = 1,
+            actual = intervals.size,
+        )
+
+        assertContentEquals(
+            expected = intArrayOf(1, 7),
+            actual = intervals[0],
+        )
+    }
+
+    @Test
+    fun `add non-overlapping interval`() {
+        manager.addInterval(intArrayOf(1, 3))
+        manager.addInterval(intArrayOf(5, 7))
+
+        val intervals = manager.getIntervals()
+
+        assertEquals(
+            expected = 2,
+            actual = intervals.size,
+        )
+
+        assertContentEquals(
+            expected = intArrayOf(1, 3),
+            actual = intervals[0],
+        )
+
+        assertContentEquals(
+            expected = intArrayOf(5, 7),
+            actual = intervals[1],
+        )
+    }
+
+    @Test
+    fun `add contiguous interval`() {
+        manager.addInterval(intArrayOf(1, 10))
+        manager.addInterval(intArrayOf(10, 20))
+
+        val intervals = manager.getIntervals()
+
+        assertEquals(
+            expected = 1,
+            actual = intervals.size,
+        )
+
+        assertContentEquals(
+            expected = intArrayOf(1, 20),
+            actual = intervals[0],
+        )
+    }
+
+    @Test
+    fun `add overlapping interval`() {
+        manager.addInterval(intArrayOf(1, 15))
+        manager.addInterval(intArrayOf(10, 30))
+
+        val intervals = manager.getIntervals()
+
+        assertEquals(
+            expected = 1,
+            actual = intervals.size,
+        )
+
+        assertContentEquals(
+            expected = intArrayOf(1, 30),
+            actual = intervals[0],
+        )
+    }
+
+    @Test
+    fun `add contained interval`() {
+        manager.addInterval(intArrayOf(1, 15))
+        manager.addInterval(intArrayOf(5, 10))
+
+        val intervals = manager.getIntervals()
+
+        assertEquals(
+            expected = 1,
+            actual = intervals.size,
+        )
+
+        assertContentEquals(
+            expected = intArrayOf(1, 15),
+            actual = intervals[0],
+        )
+    }
+
+    @Test
+    fun `add uncontained interval`() {
+        manager.addInterval(intArrayOf(30, 40))
+        manager.addInterval(intArrayOf(1, 100))
+
+        val intervals = manager.getIntervals()
+
+        assertEquals(
+            expected = 1,
+            actual = intervals.size,
+        )
+
+        assertContentEquals(
+            expected = intArrayOf(1, 100),
+            actual = intervals[0],
+        )
+    }
+
+    @Test
+    fun `add multiple intervals`() {
+        manager.addInterval(intArrayOf(75, 100))
+        manager.addInterval(intArrayOf(10, 50))
+        manager.addInterval(intArrayOf(55, 56))
+
+        val intervals = manager.getIntervals()
+
+        assertEquals(
+            expected = 3,
+            actual = intervals.size,
+        )
+
+        assertContentEquals(
+            expected = intArrayOf(10, 50),
+            actual = intervals[0],
+        )
+
+        assertContentEquals(
+            expected = intArrayOf(55, 56),
+            actual = intervals[1],
+        )
+
+        assertContentEquals(
+            expected = intArrayOf(75, 100),
+            actual = intervals[2],
         )
     }
 }
