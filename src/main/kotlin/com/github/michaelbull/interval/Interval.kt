762d37d08ed7e6f27508186c6901496f9584c474
Michael Bull <mikebull94@gmail.com>
2025-08-11 12:10:41 +0100

    Rename Interval#end to endInclusive

--- src/main/kotlin/com/github/michaelbull/interval/Interval.kt
+++ src/main/kotlin/com/github/michaelbull/interval/Interval.kt
@@ -5,27 +5,27 @@ typealias Interval = IntArray
 const val INTERVAL_SIZE = 2
 
 const val INTERVAL_START_MIN = 0
-const val INTERVAL_END_MIN = 0
+const val INTERVAL_END_INCLUSIVE_MIN = 0
 
 const val INTERVAL_START_MAX = Int.MAX_VALUE
-const val INTERVAL_END_MAX = Int.MAX_VALUE
+const val INTERVAL_END_INCLUSIVE_MAX = Int.MAX_VALUE
 
 fun checkInterval(interval: IntArray) {
     require(interval.size == INTERVAL_SIZE) {
         "interval.size must be exactly $INTERVAL_SIZE, but was ${interval.size}"
     }
 
-    val (start, end) = interval
+    val (start, endInclusive) = interval
 
     require(start >= INTERVAL_START_MIN) {
         "interval.start must be non-negative, but was $start"
     }
 
-    require(end >= INTERVAL_END_MIN) {
+    require(endInclusive >= INTERVAL_END_INCLUSIVE_MIN) {
         "interval.end must be non-negative, but was $start"
     }
 
-    require(end > start) {
-        "interval.end must be greater-than interval.start, but was start=${start} end=${end}"
+    require(endInclusive > start) {
+        "interval but be non-empty, but was [${start}..${endInclusive}]"
     }
 }
